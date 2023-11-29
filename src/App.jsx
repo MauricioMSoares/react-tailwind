26b98728e8615c9017af37adab48a6a12b3fa344
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2023-11-29 01:22:24 -0300

    feat: implemented header and native tailwindcss dark mode feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import Header from "./components/Header/Header"
+
+const App = () => {
+  return <>
+    <Header />
+  </>
 }
+
+export default App

3228118b07cc12d7e3ebcf3baba5eded4c8e0978
MauricioMSoares <mauricio.soares@sptech.school>
2023-11-29 17:55:07 -0300

    feat: added profile icons and alt texts for screen readers using tailwind sr-only

--- src/components/Petshop/Petshop.jsx
+++ src/components/Petshop/Petshop.jsx
@@ -1,8 +1,10 @@
-const Petshop = ({ name, image, bio, services }) => {
+const Petshop = ({ name, image, bio, services, alt }) => {
     return (
         <>
             <div className="p-5 bg-gray-200 dark:bg-gray-700 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
                 <h3 className="text-xl text-petsup_purple dark:text-gray-200 font-bold">{name}</h3>
+                { image && <img className="sm:p-4" src={image} />}
+                { image && alt && <span className="sr-only">{alt}</span>}
                 <div className="grid gap-1">
                     {services.map((content, index) => <p key={index} className="text-gray-900 dark:text-gray-300">{content}</p>)}
                 </div>

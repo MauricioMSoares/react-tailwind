1453f5bc0957ab1f02b0056d571bdb64e98b7cfa
MauricioMSoares <mauricio.soares@sptech.school>
2023-11-30 09:43:24 -0300

    feat: updated bio exbition to sections and customized services tags

--- src/components/Petshop/Petshop.jsx
+++ src/components/Petshop/Petshop.jsx
@@ -1,12 +1,17 @@
 const Petshop = ({ name, image, bio, services, alt }) => {
     return (
         <>
-            <div className="p-5 bg-gray-200 dark:bg-gray-700 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+            <div className="p-5 bg-gray-200 dark:bg-gray-700 sm:rounded-xl sm:shadow-lg flex flex-col items-center gap-2">
                 <h3 className="text-xl text-petsup_purple dark:text-gray-200 font-bold">{name}</h3>
-                { image && <img className="sm:p-4" src={image} />}
-                { image && alt && <span className="sr-only">{alt}</span>}
+                {image && <img className="sm:p-4" src={image} />}
+                {image && alt && <span className="sr-only">{alt}</span>}
+                <div className="hidden w-full flex-row justify-end gap-2 sm:flex">
+                    {
+                        services.map(service => <span key={service} className="bg-petsup_orange dark:bg-petsup_purple px-4 py-1 rounded-full dark:text-gray-100 text-xs uppercase font-bold">{service}</span>)
+                    }
+                </div>
                 <div className="grid gap-1">
-                    {services.map((content, index) => <p key={index} className="text-gray-900 dark:text-gray-300">{content}</p>)}
+                    {bio.map((content, index) => <p key={index} className="text-gray-900 dark:text-gray-300">{content}</p>)}
                 </div>
             </div>
         </>
